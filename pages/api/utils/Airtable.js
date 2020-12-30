920c307f2e242e9b8a0a866b0ecc293e702ecad2
ianbautista <ianfbautista@gmail.com>
2020-12-30 06:14:00 -0800

    adds Airtable helper functions

---
+++ pages/api/utils/Airtable.js
@@ -0,0 +1,17 @@
+const Airtable = require('airtable');
+const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_ID);
+
+export const table= base(process.env.AIRTABLE_TABLE_NAME)
+
+export const minifyRecords = (records) => {
+    return records.map(record => getMinifiedRecord(record));
+}
+export const getMinifiedRecord = (record => {
+    if(!record.fields.completed){
+        record.fields.completed = false;
+    }
+    return {
+        id: record.id,
+        fields: record.fields
+    }
+});
