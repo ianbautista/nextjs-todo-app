16fc82ead488db591e82e629a98fb1c7a991b3d4
ianbautista <ianfbautista@gmail.com>
2020-12-30 09:19:00 -0800

    adds delete and toggle completed functionality

--- components/Todo.js
+++ components/Todo.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { TodosContext } from "../context/TodosContext";
 import { FaTrashAlt } from "react-icons/fa";
 
 export default function Todo({ todo }) {
+	const { updateTodo, deleteTodo } = useContext(TodosContext);
+	const handleToggleCompleted = () => {
+		const updatedFields = {
+			...todo.fields,
+			completed: !todo.fields.completed,
+		};
+		const updatedTodo = { id: todo.id, fields: updatedFields };
+		updateTodo(updatedTodo);
+	};
 	return (
 		<li className="bg-white flex items-center shadow-lg rounded-lg my-2 py-2 px-4">
 			<input
@@ -10,6 +20,7 @@ export default function Todo({ todo }) {
 				id="completed"
 				checked={todo.fields.completed}
 				className="mr-2 form-checkbox h-5 w-5"
+				onChange={handleToggleCompleted}
 			/>
 			<p
 				className={`flex-1 text-gray-800 ${
@@ -19,7 +30,11 @@ export default function Todo({ todo }) {
 				{" "}
 				{todo.fields.description}{" "}
 			</p>
-			<button type="button" className="hover:text-red-700">
+			<button
+				type="button"
+				className="hover:text-red-700"
+				onClick={() => deleteTodo(todo.id)}
+			>
 				<FaTrashAlt />
 			</button>
 		</li>
