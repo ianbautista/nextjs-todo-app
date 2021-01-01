6b9b2abe704d9a230316331c5f5db695860198c6
ianbautista <ianfbautista@gmail.com>
2021-01-01 09:20:20 -0800

    styles navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,12 +3,15 @@ import React from "react";
 export default function Navbar({ user }) {
 	return (
 		<nav className="flex justify-between items-center py-4">
-			<p className="text-2xl font-bold text-grey-800"> My Todos </p>
+			<h1 className="text-4xl font-bold text-gray-50 font-mono">
+				{" "}
+				My Todos{" "}
+			</h1>
 			<div className="flex">
 				{user && (
 					<a
 						href="/api/logout"
-						className="rounded bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-4"
+						className="rounded bg-yellow-500 hover:bg-gray-100 text-gray-50 hover:text-yellow-500 py-2 px-4 font-semibold"
 					>
 						Logout
 					</a>
@@ -16,7 +19,7 @@ export default function Navbar({ user }) {
 				{!user && (
 					<a
 						href="/api/login"
-						className="rounded bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-4"
+						className="rounded bg-yellow-500 hover:bg-yellow-600 text-gray-50 py-2 px-4"
 					>
 						Login
 					</a>
