587eec95660e99a9f17c58817a348e9d469f6736
ianbautista <ianfbautista@gmail.com>
2021-01-01 07:44:07 -0800

    adds login/logout functionality

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,22 +1,26 @@
 import React from "react";
 
-export default function Navbar() {
+export default function Navbar({ user }) {
 	return (
 		<nav className="flex justify-between items-center py-4">
 			<p className="text-2xl font-bold text-grey-800"> My Todos </p>
 			<div className="flex">
-				<a
-					href="/api/logout"
-					className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4"
-				>
-					Logout
-				</a>
-				<a
-					href="/api/login"
-					className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4"
-				>
-					Login
-				</a>
+				{user && (
+					<a
+						href="/api/logout"
+						className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4"
+					>
+						Logout
+					</a>
+				)}
+				{!user && (
+					<a
+						href="/api/login"
+						className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4"
+					>
+						Login
+					</a>
+				)}
 			</div>
 		</nav>
 	);
