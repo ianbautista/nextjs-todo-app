7115f4b4615cbc0d84cd370aafddb7dd190b7725
ianbautista <ianfbautista@gmail.com>
2021-01-04 08:33:16 -0800

    fixes postcss dependency. fixes build error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-	purge: false,
+	purge: [
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"./pages/**/*.{js,ts,jsx,tsx}",
+	],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		maxWidth: {
