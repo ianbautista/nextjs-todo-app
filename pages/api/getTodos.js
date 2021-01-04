4ac144b469835a4e0f9a2a34582d78401d14c0f0
ianbautista <ianfbautista@gmail.com>
2021-01-04 07:06:45 -0800

    adds protected route

--- pages/api/getTodos.js
+++ pages/api/getTodos.js
@@ -1,15 +1,18 @@
-import {table, minifyRecords} from './utils/Airtable'
+import { table, minifyRecords } from "./utils/Airtable";
+import auth0 from "./utils/auth0";
 
-export default async  (req, res) => {
-    try {
-        const records = await table.select({}).firstPage();
-        const minifiedRecords = minifyRecords(records);
-        res.statusCode = 200;
-        res.json(minifiedRecords);
-
-    } catch (error) {
-        res.statusCode = 500;
-        res.json({msg: `Oops! ..something went wrong my friend. Here's what happened: ${error}`});
-    }
-
-    };
+export default auth0.requireAuthentication(async (req, res) => {
+	try {
+		const records = await table
+			.select({ filterByFormula: `userid = '${session.user.name}'` })
+			.firstPage();
+		const minifiedRecords = minifyRecords(records);
+		res.statusCode = 200;
+		res.json(minifiedRecords);
+	} catch (error) {
+		res.statusCode = 500;
+		res.json({
+			msg: `Oops! ..something went wrong my friend. Here's what happened: ${error}`,
+		});
+	}
+});
