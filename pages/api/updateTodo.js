c9434eb7c78868cebb254aad94558f57ba3c52bc
ianbautista <ianfbautista@gmail.com>
2021-01-04 07:07:54 -0800

    adds middleware to update route

--- pages/api/updateTodo.js
+++ pages/api/updateTodo.js
@@ -1,19 +1,16 @@
-import {table, getMinifiedRecord} from './utils/Airtable'
+import { table, getMinifiedRecord } from "./utils/Airtable";
+import ownsRecord from "./middleware/OwnsRecord";
 
-export default async  (req, res) => {
-    const {id, fields} = req.body;
-
-    try {
-        const updatedRecord = await table.update([
-            {id, fields}
-        ]);
-
-        res.statusCode = 200;
-        res.json(getMinifiedRecord(updatedRecord[0]));
-
-    } catch (error) {
-        res.statusCode = 500;
-        res.json({msg: `Oops! ..something went wrong my friend. Here's what happened: ${error}`});
-    }
-
-    };
+export default ownsRecord(async (req, res) => {
+	const { id, fields } = req.body;
+	try {
+		const updatedRecord = await table.update([{ id, fields }]);
+		res.statusCode = 200;
+		res.json(getMinifiedRecord(updatedRecord[0]));
+	} catch (error) {
+		res.statusCode = 500;
+		res.json({
+			msg: `Oops! ..something went wrong my friend. Here's what happened: ${error}`,
+		});
+	}
+});
