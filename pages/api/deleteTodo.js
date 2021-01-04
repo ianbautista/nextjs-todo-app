d9523e7adfcca1f603190376703e821bb5c7dcc9
ianbautista <ianfbautista@gmail.com>
2021-01-04 07:05:21 -0800

    adds middleware to delete route

--- pages/api/deleteTodo.js
+++ pages/api/deleteTodo.js
@@ -1,17 +1,18 @@
-import {table, getMinifiedRecord} from './utils/Airtable'
+import { table, getMinifiedRecord } from "./utils/Airtable";
+import ownsRecord from "./middleware/OwnsRecord";
 
-export default async  (req, res) => {
-    const {id} = req.body;
+export default ownsRecord(async (req, res) => {
+	const { id } = req.body;
 
-    try {
-        const deletedTodo = await table.destroy([id]);
+	try {
+		const deletedTodo = await table.destroy([id]);
 
-        res.statusCode = 200;
-        res.json(getMinifiedRecord(deletedTodo[0]));
-
-    } catch (error) {
-        res.statusCode = 500;
-        res.json({msg: `Oops! ..something went wrong my friend. Here's what happened: ${error}`});
-    }
-
-    };
+		res.statusCode = 200;
+		res.json(getMinifiedRecord(deletedTodo[0]));
+	} catch (error) {
+		res.statusCode = 500;
+		res.json({
+			msg: `Oops! ..something went wrong my friend. Here's what happened: ${error}`,
+		});
+	}
+});
