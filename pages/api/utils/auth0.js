37a756ed3106ce21a6b842a4244d260298fd7ff6
ianbautista <ianfbautista@gmail.com>
2021-01-04 09:02:19 -0800

    updates callback and uri

--- pages/api/utils/auth0.js
+++ pages/api/utils/auth0.js
@@ -5,8 +5,8 @@ export default initAuth0({
 	clientId: process.env.AUTH0_CLIENT_ID,
 	clientSecret: process.env.AUTH0_SECRET,
 	scope: "openid profile",
-	redirectUri: "http://localhost:3000/api/callback",
-	postLogoutRedirectUri: "http://localhost:3000/",
+	redirectUri: "https://mysimpletodoapp.vercel.app/api/callback",
+	postLogoutRedirectUri: "https://mysimpletodoapp.vercel.app/",
 	session: {
 		// The secret used to encrypt the cookie.
 		cookieSecret: process.env.COOKIE_SECRET,
