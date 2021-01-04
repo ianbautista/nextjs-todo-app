d265357bc401f313dcf36b87ea110c7be40730cb
ianbautista <ianfbautista@gmail.com>
2021-01-04 05:16:49 -0800

    styles app container

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,10 @@ import { TodosProvider } from "../context/TodosContext";
 function MyApp({ Component, pageProps }) {
 	return (
 		<TodosProvider>
-			<div className="container mx-auto max-w-xl h-screen bg-yellow-500">
-				<Component {...pageProps} />
+			<div className=" overflow-hidden flex mx-auto w-screen h-screen bg-yellow-400 py-4 px-4 box-border">
+				<div className=" overflow-hidden overflow-y-auto max-w-xl mx-auto h-full iPhonePlus:w-full iPhoneX:w-full bg-yellow-400 border-gray-50 border-8 rounded-3xl">
+					<Component {...pageProps} />
+				</div>
 			</div>
 		</TodosProvider>
 	);
