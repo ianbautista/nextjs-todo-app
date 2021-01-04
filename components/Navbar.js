0b804a10ce3afaf3363129f03c2d7b85d2afad2b
ianbautista <ianfbautista@gmail.com>
2021-01-04 05:17:52 -0800

    styles navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Navbar({ user }) {
 	return (
-		<nav className="flex justify-between items-center py-4">
+		<nav className="flex justify-between items-center py-4 iPhoneX:px-2 iPhonePlus:px-0">
 			<h1 className="text-4xl font-bold text-gray-50 font-mono">
 				{" "}
 				My Todos{" "}
@@ -11,7 +11,7 @@ export default function Navbar({ user }) {
 				{user && (
 					<a
 						href="/api/logout"
-						className="rounded bg-yellow-500 hover:bg-gray-100 text-gray-50 hover:text-yellow-500 py-2 px-4 font-semibold"
+						className="rounded bg-yellow-400 hover:bg-gray-100 text-gray-50 hover:text-yellow-400 py-2 px-4 font-semibold"
 					>
 						Logout
 					</a>
@@ -19,7 +19,7 @@ export default function Navbar({ user }) {
 				{!user && (
 					<a
 						href="/api/login"
-						className="rounded bg-yellow-500 hover:bg-yellow-600 text-gray-50 py-2 px-4"
+						className="rounded bg-yellow-400 hover:bg-gray-100 text-gray-50 hover:text-yellow-400 py-2 px-4 font-semibold"
 					>
 						Login
 					</a>
