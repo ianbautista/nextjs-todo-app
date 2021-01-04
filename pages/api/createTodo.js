e6a21559b1ced0c5fee40561295861846cfc9ad6
ianbautista <ianfbautista@gmail.com>
2021-01-04 05:14:09 -0800

    adds the user name to the to do on create

--- pages/api/createTodo.js
+++ pages/api/createTodo.js
@@ -1,20 +1,24 @@
-import {table} from './utils/Airtable'
+import { table } from "./utils/Airtable";
+import auth0 from "./utils/auth0";
 
-export default async  (req, res) => {
-    const {description} = req.body;
+export default auth0.requireAuthentication(async (req, res) => {
+	const { user } = await auth0.getSession(req);
+	const { description } = req.body;
 
-    try {
-        const createdRecords = await table.create([{fields: {description}}]);
-        const createdRecord = {
-            id: createdRecords[0].id,
-            fields: createdRecords[0].fields
-        }
-        res.statusCode = 200;
-        res.json(createdRecord);
-
-    } catch (error) {
-        res.statusCode = 500;
-        res.json({msg: `Oops! ..something went wrong my friend. Here's what happened: ${error}`});
-    }
-
-    };
+	try {
+		const createdRecords = await table.create([
+			{ fields: { description, userid: user.name } },
+		]);
+		const createdRecord = {
+			id: createdRecords[0].id,
+			fields: createdRecords[0].fields,
+		};
+		res.statusCode = 200;
+		res.json(createdRecord);
+	} catch (error) {
+		res.statusCode = 500;
+		res.json({
+			msg: `Oops! ..something went wrong my friend. Here's what happened: ${error}`,
+		});
+	}
+});
