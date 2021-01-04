8bebdff7486364a1cc3335162d3a8fb69ef7f0ac
ianbautista <ianfbautista@gmail.com>
2021-01-04 12:07:03 -0800

    updates to PWA

--- pages/_document.js
+++ pages/_document.js
@@ -10,22 +10,30 @@ class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-					<title>My To Do App</title>
-					<link
-						rel="icon"
-						href="https://mysimpletodoapp.now.sh/todo.png"
-					/>
-					<meta
-						name="viewport"
-						content="width=device-width, initial-scale=1"
-					/>
+					<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 					<meta name="theme-color" content="#000000" />
 					<meta
 						name="description"
 						content="Todo App made by Christian Bautista"
 					/>
-					<link rel="apple-touch-icon" href="../public/favicon.png" />
-					<link rel="manifest" href="/manifest.json" />
+					<meta
+						name="keywords"
+						content="to, do, app, todo, next, christian, bautista, auth0, web, pwa"
+					/>
+					<meta name="theme-color" content="#000000" />
+					<meta name="mobile-web-app-capable" content="yes" />
+					<meta
+						name="apple-mobile-web-app-title"
+						content="My To Do App"
+					/>
+					<meta name="apple-mobile-web-app-capable" content="yes" />
+					<meta
+						name="apple-mobile-web-app-status-bar-style"
+						content="default"
+					/>
+					<link rel="icon" href="favicon.png" />
+					<link rel="apple-touch-icon" href="favicon.png" />
+					<link rel="manifest" href="manifest.json" />
 				</Head>
 				<body>
 					<Main />
